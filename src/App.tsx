968fb87463f9bb7f9bbd87d12fc21ea44e083931
Tam Bui <tam@buzzbike.london>
2022-05-17 16:36:17 +0700

    feat: initial commit with home page ui

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+export default function App() {
+  const topFive = ["GrahamCampbell","fabpot","weierophinney","rkh","josh"]
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Top 5 GitHub users
+        </Typography>
+        <Typography variant="body1" component="p" gutterBottom>
+          Tap the username to see more information
+        </Typography>
+
+        <Box display="flex" gap={1} mt={2}>
+        {topFive.map(user => (
+          <Button variant="contained">{user}</Button>
+        ))}
+        </Box>
+      </Box>
+    </Container>
+  );
+}
